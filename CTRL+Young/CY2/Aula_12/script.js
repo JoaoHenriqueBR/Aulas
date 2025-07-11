e08de28dd17d8e3eaaafc3eeb3dafee1ede99623
JoaoHenriqueBR <jjb9362@gmail.com>
2025-07-11 20:01:04 -0300

    Aula 12 - Páginas mais interativas com JavaScript

---
+++ CTRL+Young/CY2/Aula_12/script.js
@@ -0,0 +1,28 @@
+// Comentário muito legal wow
+document.writeln("<h1>São Paulo FC</h1>");
+
+/*
+Comentário de várias linhas
+
+aaaa
+*/
+num1 = 35645437825627
+num2 = 234
+
+Soma = num1 + num2
+
+
+Multiplica = num1*num2
+
+document.writeln(Multiplica)
+document.writeln(Soma)
+
+var x;
+var a, b, c;
+var alfa = 0, beta = 100;
+
+var idade = null;
+var maisVelho = 3 * idade; // NaN
+
+document.writeln(maisVelho)
+
