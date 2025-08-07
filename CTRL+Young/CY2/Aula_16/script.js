399758a1819a38f90f5a48a50481c1701a85d15b
JoaoHenriqueBR <jjb9362@gmail.com>
2025-08-07 20:54:11 -0300

    Aula 16 - A Jornada da interatividade

---
+++ CTRL+Young/CY2/Aula_16/script.js
@@ -0,0 +1,40 @@
+/*
+let pao = 10;
+const pi = 3.1448923749182;
+
+if (true) {
+    let pao = 5
+}
+
+console.log(pi)
+
+bool = true // Boolean 
+nulo = null // nulo
+let qualquercoisa;
+numero = 10
+string = "olha que palavra bonita"
+objeto = {"Escola":"Ctrl+Play"}
+
+document.write(resultado)
+console.log(resultado)
+print() */
+
+let n = parseInt(prompt('Digite um número: '))
+// n = int(input('Digite um número: '))
+
+if (n == 1){
+    document.writeln('Impar')
+} else{
+    if (n % 2 == 0){
+    document.writeln('Par')
+    } else {
+    document.writeln('Impar')
+    }
+}
+
+let a = 3;
+let b = 3.14;
+let c = true;
+let d = "10";
+let e = null;
+let f = undefined;
