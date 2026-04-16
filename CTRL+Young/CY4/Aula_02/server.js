f33f343b041829eb10abcb1eb0789b11daa225d3
JoaoHenriqueBR <jjb9362@gmail.com>
2026-04-16 20:40:25 -0300

    Aula 02 - Introdução ao CRUD
    PROJETO: Criar nosso próprio catálogo de filmes usando o CRUD
    
    - Introdução (5 min)
    - Explorando os Fundamentos (15 min)
    	- O que é CRUD?
    - Cultura Digital (10 min)
            - O que são metadados?
    	- História do IMDb
    	- Curiosidades
    - Implementando o CRUD no Console (60 min)
    	- Passo 1: Preparando o Ambiente
    	- Passo 2: Criando um Filme (Create)
    	- Passo 3: Listando Todos os Filmes (Read)
    	- Passo 4: Atualizando um Filme (Update)
    	- Passo 5: Removendo um Filme (Delete)
    - Conclusão e Revisão
    	- Atividades e Desafio
    
    PRÓXIMA AULA: Na próxima aula, você vai expandir suas rotas e explorar outros métodos HTTP, como POST, PUT e DELETE, para completar as operações CRUD no servidor!

---
+++ CTRL+Young/CY4/Aula_02/server.js
@@ -0,0 +1,30 @@
+// Create
+let movies = [
+    { id:1, title: "O Senhor dos Anéis", genre: "Fantasia", year: 2001},
+    { id:2, title: "Matrix", genre: "Ficção Científica", year: 1999}
+];
+
+movies.push({id:3, title: "Avatar", genre: "Ficção Científica", year: 2009})
+
+console.log(movies);
+
+// Read
+movies.forEach(movie => {
+    console.log(`Título: ${movie.title}, Gênero: ${movie.genre}, Ano: ${movie.year}`);
+});
+
+// Update
+let movieToUpdate = movies.find(movie => movie.id === 1)
+if (movieToUpdate) {
+    movieToUpdate.genre = "Aventura";
+    console.log(`Filme atualizado: ${movieToUpdate.title}, Gênero: ${movieToUpdate.genre}`);
+}
+
+// Delete
+let index = movies.findIndex(movie => movie.id === 3);
+if (index !== -1){
+    movies.splice(index, 1);
+    console.log("Filme removido!");
+}
+
+console.log(movies)
