e1bdd582ef2c906a24c4936e7acb0fb427a64005
BressaniEND <70080335+JoaoHenriqueBR@users.noreply.github.com>
2026-06-15 18:38:01 -0300

    Aula 05 - Novas Aventuras com React.js
    Vimos React.js e Node.js, bibliotecas de JavaScript que simplifica e permite fazer alterações maiores a Apps e Sites, com o Node.js permitindo o uso para tudo no sistema, inclusive criar Apps. E o React.js simplificando e criando funções que tornam ações complexas de HTML/CSS/JS simplificadas em comandos únicos e responsivos.

---
+++ CTRL+Young/CY3/Aula_05/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://images.seeklogo.com/logo-png/25/3/confederacao-brasileira-de-futebol-logo-png_seeklogo-252745.png "className="App-logo" alt="logo" />
+        <h1>RUMO AO HEXA!!!</h1>
+        <p>
+          VAMOS BRASIL!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
