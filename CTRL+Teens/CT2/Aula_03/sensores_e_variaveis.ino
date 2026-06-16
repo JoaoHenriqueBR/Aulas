6e60ebbfdaa4e3d9f539e4d178e4f85e068e14ac
BressaniEND <70080335+JoaoHenriqueBR@users.noreply.github.com>
2026-06-16 20:13:15 -0300

    Aula 03 - Sensores e variáveis
    📚Conteúdo da aula de hoje: Nesta sexta vimos Robótica com Arduino, onde revisamos a criação de um Semáforo automatizado e exploramos o uso de sensores para medir distâncias, assim criando um projeto de uma "Trena Sem Fio" que mede a distância entre um Sensor Ultrassônico e qualquer objeto entre 2cm e 4m.
    ⭐Link Extras: (Projeto) https://www.tinkercad.com/things/196Q87h8EPR-sensores-e-variaveis
    (Video Tutorial) https://drive.google.com/file/d/1tVDe5RT9IQuhYw2sOKu61TBLOzw9T4Hl/view?usp=drive_link

---
+++ CTRL+Teens/CT2/Aula_03/sensores_e_variaveis.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  cm = 0.01723 * readUltrasonicDistance(8, 7);
+  Serial.print(cm);
+  Serial.println("cm");
+  delay(1000); // Wait for 1000 millisecond(s)
+}
